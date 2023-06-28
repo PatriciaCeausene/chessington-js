2eceb88bf72c62c0f1960b691d41fa26c9712993
PatriciaCeausene <Oana.Ceausene@softwire.com>
2023-06-28 17:27:13 +0300

    Made prawns move 1 or 2 squares

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -8,11 +8,28 @@ export default class Pawn extends Piece {
         super(player);
     }
 
+    public isItFirstMove(board: Board): boolean {
+        if (this.player === Player.WHITE && board.findPiece(this).row == 1) {
+            return true;
+        }
+        if (this.player === Player.BLACK && board.findPiece(this).row == 6) {
+            return true;
+        }
+        return false;
+    }
     public getAvailableMoves(board: Board) {
         if (this.player === Player.WHITE) {
-            return Square.at(board.findPiece(this).row + 1,board.findPiece(this).col);
+            if (this.isItFirstMove(board)) {
+                return [Square.at(board.findPiece(this).row + 1,board.findPiece(this).col),
+                    Square.at(board.findPiece(this).row + 2,board.findPiece(this).col)]
+            }
+            return [Square.at(board.findPiece(this).row + 1,board.findPiece(this).col)];
         } else {
-            return Square.at(board.findPiece(this).row - 1,board.findPiece(this).col);
+            if (this.isItFirstMove(board)) {
+                return [Square.at(board.findPiece(this).row - 1,board.findPiece(this).col),
+                    Square.at(board.findPiece(this).row - 2,board.findPiece(this).col)]
+            }
+            return [Square.at(board.findPiece(this).row - 1,board.findPiece(this).col)];
         }
     }
 }
