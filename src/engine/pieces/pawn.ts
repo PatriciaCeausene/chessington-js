fb2b388feddf83083d8e90d154b89a270b9324a2
PatriciaCeausene <Oana.Ceausene@softwire.com>
2023-06-28 16:22:29 +0300

    Made prawns move up and down.

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,10 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        if (this.player === Player.WHITE) {
+            return Square.at(board.findPiece(this).row + 1,board.findPiece(this).col);
+        } else {
+            return Square.at(board.findPiece(this).row - 1,board.findPiece(this).col);
+        }
     }
 }
